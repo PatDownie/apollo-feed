dc87dbf2fcfa9cd8e2efb260b668d3835b9b0fbb
Patrick Downie <patdownie07@gmail.com>
2023-05-22 16:15:59 +0000

    all critical functionality is functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import Home from "./pages/Home";
 import SongGenerate from "./pages/SongGenerate";
 import Forum from "./pages/Forum";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Songs</h1>
+        <header>
+          <h1>Apollo Feed</h1>
+          <h2>For all of your needs</h2>
+          <div>
+            <img src="https://placehold.co/100" />
+            <ul>
+              <li>
+                <NavLink to={`/home`}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={`/feed`}>Feed</NavLink>
+              </li>
+              <li>
+                <NavLink to={`/forum`}>Forum</NavLink>
+              </li>
+            </ul>
+          </div>
+        </header>
         <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/SongGenerate" element={<SongGenerate />} />
-          <Route path="/Forum" element={<Forum />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/feed" element={<SongGenerate />} />
+          <Route path="/forum" element={<Forum />} />
         </Routes>
       </div>
     </BrowserRouter>
